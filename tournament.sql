1a41a1f61541862affec68c8f000569fed340ac8
Juancabrito <juancarlos@taxiconfiable.com>
2016-05-29 13:31:18 -0500

    Current code, bad pairing

--- tournament.sql
+++ tournament.sql
@@ -4,61 +4,53 @@ DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
 \c tournament
 
-CREATE TABLE Tournaments (
-  active boolean,
-  id serial PRIMARY KEY
-);
 
-CREATE TABLE Players (
+CREATE TABLE RegisteredPlayers (
   name text,
   id serial PRIMARY KEY
 );
 
-CREATE TABLE RegisteredPlayers (
-  player  int REFERENCES Players(id),
-  tournament  int REFERENCES Tournaments(id)
-);
-
 CREATE TABLE Matches (
-  winner_id  int REFERENCES Players(id),
-  loser_id  int REFERENCES Players(id),
-  tournament  int REFERENCES Tournaments(id),
-  id serial PRIMARY KEY
+  winner_id  int REFERENCES RegisteredPlayers(id),
+  loser_id  int REFERENCES RegisteredPlayers(id),
+  round  int
 );
 
-CREATE VIEW v_registered_players AS
-  SELECT RegisteredPlayers.player, Players.name, RegisteredPlayers.tournament
-  FROM RegisteredPlayers JOIN Players
-  ON RegisteredPlayers.player = Players.id
-  ORDER BY tournament ASC;
+--CREATE VIEW v_matches_count AS
+--  SELECT Players.id,
+--  SUM(
+--    CASE WHEN Matches.winner_id = Players.id
+--    OR Matches.loser_id = Players.id THEN 1 ELSE 0 END
+--  ) AS matches
+--  FROM Players LEFT JOIN Matches
+--  ON Matches.winner_id = Players.id OR Matches.loser_id = Players.id
+--  JOIN RegisteredPlayers ON RegisteredPlayers.player = Players.id
+--  GROUP BY Players.id;
 
 CREATE VIEW v_matches_count AS
-  SELECT Players.id, count(Matches.id) AS matches, RegisteredPlayers.tournament
-  FROM Players LEFT JOIN Matches
-  ON Matches.winner_id = Players.id OR Matches.loser_id = Players.id
-  JOIN RegisteredPlayers ON RegisteredPlayers.player = Players.id AND RegisteredPlayers.tournament = Matches.tournament
-  GROUP BY Players.id, RegisteredPlayers.tournament
-  ORDER BY tournament ASC;
+  SELECT RegisteredPlayers.name,
+  SUM(
+    CASE WHEN Matches.winner_id = RegisteredPlayers.id OR Matches.loser_id = RegisteredPlayers.id
+    THEN 1 ELSE 0 END
+  ) AS matches
+  FROM RegisteredPlayers LEFT JOIN Matches
+  ON Matches.winner_id = RegisteredPlayers.id OR Matches.loser_id = RegisteredPlayers.id
+  GROUP BY RegisteredPlayers.name;
 
 CREATE VIEW v_score AS
-  SELECT Players.id, Players.name, count(Matches.winner_id) AS wins, RegisteredPlayers.tournament
-  FROM Players LEFT JOIN Matches
-  ON Matches.winner_id = Players.id
-  JOIN RegisteredPlayers ON RegisteredPlayers.player = Players.id
-  WHERE RegisteredPlayers.tournament = Matches.tournament
-  GROUP BY Players.id, RegisteredPlayers.tournament
-  ORDER BY tournament ASC;
+  SELECT RegisteredPlayers.id, RegisteredPlayers.name,
+  SUM(
+    CASE WHEN Matches.winner_id = RegisteredPlayers.id
+    THEN 1 ELSE 0 END
+  ) AS wins
+  FROM RegisteredPlayers LEFT JOIN Matches
+  ON Matches.winner_id = RegisteredPlayers.id OR Matches.loser_id = RegisteredPlayers.id
+  GROUP BY RegisteredPlayers.id;
 
 CREATE VIEW v_standings AS
   SELECT v_score.id, v_score.name, v_score.wins,
-  v_matches_count.matches, v_score.tournament
-  FROM v_score JOIN v_matches_count
-  ON v_score.tournament = v_matches_count.tournament AND v_score.id = v_matches_count.id
-  ORDER BY tournament ASC;
+  v_matches_count.matches
+  FROM v_score LEFT JOIN v_matches_count
+  ON v_score.name = v_matches_count.name
+  ORDER BY wins DESC;
 
-CREATE VIEW v_tournaments AS
-  SELECT v_standings.id, v_standings.name, v_standings.wins,
-  v_standings.matches, v_standings.tournament, Tournaments.active
-  FROM v_standings JOIN Tournaments
-  ON v_standings.tournament = Tournaments.id
-  ORDER BY tournament ASC;
