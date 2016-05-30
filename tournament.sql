2337e784f6f2633a10fa8a3e61638d14b652614f
Juancabrito <juancarlos@taxiconfiable.com>
2016-05-29 21:51:40 -0500

    Change the schema to make the code work

--- tournament.sql
+++ tournament.sql
@@ -4,34 +4,41 @@ DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
 \c tournament
 
-CREATE TABLE Players (
+
+CREATE TABLE RegisteredPlayers (
   name text,
   id serial PRIMARY KEY
 );
 
 CREATE TABLE Matches (
-  winner_id  int REFERENCES Players(id),
-  loser_id  int REFERENCES Players(id),
-  id serial PRIMARY KEY
+  winner_id  int REFERENCES RegisteredPlayers(id),
+  loser_id  int REFERENCES RegisteredPlayers(id),
+  round  int
 );
 
--- View for counting matches by player.
 CREATE VIEW v_matches_count AS
-	SELECT Players.id, count(Matches.id) AS matches
-	FROM Players LEFT JOIN Matches
-  ON Matches.winner_id = Players.id OR Matches.loser_id = Players.id
-	GROUP BY Players.id ORDER BY matches;
+  SELECT RegisteredPlayers.name,
+  SUM(
+    CASE WHEN Matches.winner_id = RegisteredPlayers.id OR Matches.loser_id = RegisteredPlayers.id
+    THEN 1 ELSE 0 END
+  ) AS matches
+  FROM RegisteredPlayers LEFT JOIN Matches
+  ON Matches.winner_id = RegisteredPlayers.id OR Matches.loser_id = RegisteredPlayers.id
+  GROUP BY RegisteredPlayers.name;
 
--- View for computing wins by player.
 CREATE VIEW v_score AS
-  SELECT Players.id, Players.name, count(Matches.winner_id) AS wins
-  FROM Players LEFT JOIN Matches
-  ON Matches.winner_id = Players.id
-  GROUP BY Players.id ORDER BY wins DESC;
+  SELECT RegisteredPlayers.id, RegisteredPlayers.name,
+  SUM(
+    CASE WHEN Matches.winner_id = RegisteredPlayers.id
+    THEN 1 ELSE 0 END
+  ) AS wins
+  FROM RegisteredPlayers LEFT JOIN Matches
+  ON Matches.winner_id = RegisteredPlayers.id OR Matches.loser_id = RegisteredPlayers.id
+  GROUP BY RegisteredPlayers.id;
 
--- View for standings, including player's id, name, wins and matches played.
 CREATE VIEW v_standings AS
-  SELECT v_score.id, v_score.name, v_score.wins, v_matches_count.matches
-  FROM v_score JOIN v_matches_count
-  ON v_score.id = v_matches_count.id
-	ORDER BY wins DESC;
+  SELECT v_score.id, v_score.name, v_score.wins,
+  v_matches_count.matches
+  FROM v_score LEFT JOIN v_matches_count
+  ON v_score.name = v_matches_count.name
+  ORDER BY wins DESC;
